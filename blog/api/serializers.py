4bca46fa2fe6a91238cb9908b205d1576c99f891
AlexanderPAI <lawlikeart@yandex.ru>
2024-02-06 01:23:58 +0300

    Feat: field is_read has been added to PostSerializer

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -6,8 +6,16 @@ from posts.models import IsRead, Post
 class PostSerializer(serializers.ModelSerializer):
     """Сериализатор модели Post."""
     author = serializers.StringRelatedField()
+    is_read = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ('__all__')
+        fields = '__all__'
         read_only_fields = ('author',)
+
+    def get_is_read(self, obj):
+        """Метод для отображения поля поста 'Прочитано'."""
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return obj.is_read.filter(user=user)
