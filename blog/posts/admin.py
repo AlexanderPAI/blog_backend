eb42239e4b610b53729e085a523e63ce6192eb23
AlexanderPAI <lawlikeart@yandex.ru>
2024-02-05 23:39:56 +0300

    Feat: Post model has been created

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'title',
+    )
+    readonly_fields = ('pub_date',)
+
+
+admin.site.register(Post, PostAdmin)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    """Модель поста."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор поста',
+    )
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Заголовок',
+        help_text='Введите заголовок поста',
+    )
+    text = models.TextField(
+        max_length=140,
+        verbose_name='Текст поста',
+        help_text='Напишите содержимое поста',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата создания поста',
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.title
