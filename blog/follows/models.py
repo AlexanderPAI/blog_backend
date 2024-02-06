2875076f4872fea3641cdf610374aa7b9ab1e07d
AlexanderPAI <lawlikeart@yandex.ru>
2024-02-06 15:38:26 +0300

    Feat: Follow model has been created

--- blog/follows/models.py
+++ blog/follows/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Follow(models.Model):
+    """Модель подписки."""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'author'),
+                name='unique_follow',
+            ),
+        )
+
+    def __str__(self):
+        return f'Подписка f{self.user.username} -> f{self.author.username}'
