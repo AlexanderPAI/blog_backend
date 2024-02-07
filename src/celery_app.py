2dd77503b3a6efff540871b847a6a00ae0320937
AlexanderPAI <lawlikeart@yandex.ru>
2024-02-07 03:54:30 +0300

    Feat: celery settings

---
+++ src/celery_app.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+
+app = Celery('blog')
+app.config_from_object('django.conf:settings')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
+
+
+@app.task()
+def debug_task():
+    time.sleep(20)
+    print('Hello from debug_task')
